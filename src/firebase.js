4db8c2ba12fb92bf1dba2b5eea6946705a35cbc5
Roselyn <roselynh100@gmail.com>
2022-04-15 22:10:45 -0400

    added environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import "firebase/firestore";
 //import { getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: "uwu-orbital.firebaseapp.com",
-    projectId: "uwu-orbital",
-    storageBucket: "uwu-orbital.appspot.com",
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID 
+};
 
 //const firebase = Firebase.initializeApp(firebaseConfig);
 
