f65c8688d826aa8a43acbdfeabb3661076e12928
WeilanTao <twlemily@hotmail.com>
2022-08-23 16:30:40 -0400

    remove something trivial

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,7 +6,7 @@ import { getFirestore } from "@firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-console.log(process.env);
+// console.log(process.env);
 const firebaseConfig = {
   apiKey: `${process.env.REACT_APP_API_KEY}`,
   authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
